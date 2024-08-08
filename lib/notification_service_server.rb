08c9b751f86b89f0075ce74d9ad9c5e0a3cdad3f
marcioHcastilho <marciocastilho@bemprotege.com.br>
2024-08-08 11:43:12 -0300

    remove bug protoc
    resolvendo bug nao estava armazenando no banco de dados notifications os dados do usuario

--- lib/notification_service_server.rb
+++ lib/notification_service_server.rb
@@ -1,27 +1,35 @@
 require 'grpc'
-require_relative 'notification_services_pb'
+require_relative '../lib/notification_services_pb'
 require_relative '../config/environment'
-
 
 class NotificationServiceServer < Notification::NotificationService::Service
   def send_notification(notification_request, _unused_call)
     puts "Received notification request: #{notification_request.inspect}"
-
-    result = NotificationService.create_notification(
-      action: notification_request.action,
-      user_id: notification_request.user_id,
-      user_email: notification_request.user_email,
-      task_id: notification_request.task_id,
-      task_title: notification_request.task_title,
-      task_description: notification_request.task_description
-    )
-
-    Notification::NotificationResponse.new(success: result[:success], message: result[:message])
+    create_notification(notification_request)
   end
-
   def send_webscraping_notification(webscraping_notification_request, _unused_call)
     puts "Received webscraping notification request: #{webscraping_notification_request.inspect}"
+    
     Notification::NotificationResponse.new(success: true, message: 'Webscraping notification received')
+  end
+  private
+
+  def create_notification(notification_request)
+    begin
+      UserNotification.create!(
+        action: notification_request.action,
+        user_id: notification_request.user_id,
+        user_email: notification_request.user_email,
+        task_id: notification_request.task_id,
+        task_title: notification_request.task_title,
+        task_description: notification_request.task_description
+      )
+      puts "Notification created successfully."
+      Notification::NotificationResponse.new(success: true, message: 'Notification received')
+    rescue => e
+      puts "Failed to create notification: #{e.message}"
+      Notification::NotificationResponse.new(success: false, message: e.message)
+    end
   end
 end
 
@@ -32,6 +40,8 @@ def start_server
   puts "Server is listening on port 50052"
   s.run_till_terminated
 end
+
+start_server
 
 start_server
 
