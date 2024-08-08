0dadb5a1d7ba06e026b05e4721801e809fd27cb5
marcioHcastilho <marciocastilho@bemprotege.com.br>
2024-08-08 11:34:15 -0300

    resolving bug protoc

--- app/service/notification_service.rb
+++ app/service/notification_service.rb
@@ -2,14 +2,17 @@ require 'grpc'
 require_relative '../../lib/notification_services_pb'
 
 class NotificationServiceImpl < Notification::NotificationService::Service
-  def send_notification(request, _unused_call)
+  def send_notification(request)
     notification_params = {
+      action: request.action
+      user_id: request.user_id,
+      user_email: request.user_email,
       task_id: request.task_id,
       task_title: request.task_title,
       task_description: request.task_description,
-      user_id: request.user_id,
-      user_email: request.user_email,
-      action: request.action
+      
+      
+     
     }
 
     notification = NotificationRepository.create(notification_params)
@@ -25,7 +28,7 @@ end
 # Inicializa o servidor gRPC
 def start_grpc_server
   server = GRPC::RpcServer.new
-  server.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
+  server.add_http2_port('0.0.0.0:50052', :this_port_is_insecure)
   server.handle(NotificationServiceImpl)
   server.run_till_terminated
 end
